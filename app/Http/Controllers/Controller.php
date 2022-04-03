8392138ba06a952fc5b80dde22a08a1747331ae8
alvaro19jnl <80657010+alvaro19jnl@users.noreply.github.com>
2022-04-03 07:36:58 -0500

    Add files via upload

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+public function store(Request $request)
+{
+if($request->hastfile('image')){
+    $path=$request->image->store('public');
+    Image::create(['path'=>$path]);
+}
+
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,8 @@ Route::view('login', 'login')->name('login');
 Route::view('plataform', 'plataform')->name('plataform');
 Route::view('upimage', 'upimage')->name('upimage');
 Route::post('/subir','Controller@subirArchivo')->name('subir');
-
+Route::view('perfil', 'perfil')->name('perfil');
 Route::post('/fileUpload', [ImagenDroneController::class, 'create']);
+Route::view('register', 'register')->name('register');
+Route::view('eventos', 'eventos')->name('eventos');
+Route::view('validate','validate')->name('validate');
