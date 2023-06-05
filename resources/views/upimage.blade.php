baa5d3e48dca507631f8c6bc439fce9f86cc5011
alvaro19jnl <80657010+alvaro19jnl@users.noreply.github.com>
2023-06-05 11:16:17 -0500

    Cambios en la visualización de las imagenes

--- resources/views/upimage.blade.php
+++ resources/views/upimage.blade.php
@@ -29,9 +29,25 @@ Subida de imagenes
 </div>
 <div class="card__body">
           <img src="https://www5.djicdn.com/cms/uploads/2808f1b40ded0555f0378af97cd59a45.png"  class="card__image">
-          <h1 class="card__title ">Vista Previa</h1>
+          
         </div>
-        <div id="previewImagesDiv"></div>
-      </div>
+
+        <h1 class="card__title ">Vista Previa</h1>
+
+
+        <div class="zoom">
+        <div id=alfa>
+      
+<div id=mk0></div><div id=mk1></div><div id=mk2></div>
+<div id=mk3></div><div id=mk4></div><div id=mk5></div>
+<div id=mk6></div><div id=mk7></div><div id=mk8></div>
+
+
+
+  <div  id="previewImagesDiv"></div>
+  </div>
+  </div>
+  </div>
+
       @csrf
 @endsection
